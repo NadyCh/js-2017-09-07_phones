b8d8cb921f6f80d96d8b4ea9d73a145430e86815
NadyCh <nady.smb@gmail.com>
2017-11-02 12:43:42 +0300

    xhr replaced with fetch

--- js/services/http-service.js
+++ js/services/http-service.js
@@ -1,31 +1,31 @@
-const API_BASE_URL = 'http://localhost:8080/server/data/';
+//const API_BASE_URL = 'http://localhost:8080/server/data/';
 
 class HttpService {
   static sendRequest(url, { method = 'GET' } = {}) {
 
 
-      return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
 
-        let xhr = new XMLHttpRequest();
+      let xhr = new XMLHttpRequest();
 
-        xhr.open(method, API_BASE_URL + url, true);
+      xhr.open(method, API_BASE_URL + url, true);
 
-        xhr.onload = () => {
-          if (xhr.status !== 200) {
-            reject( xhr.status + ': ' + xhr.statusText );
-          } else {
-            let data = JSON.parse(xhr.responseText);
-
-            resolve(data);
-          }
-        };
-
-        xhr.onerror = () => {
+      xhr.onload = () => {
+        if (xhr.status !== 200) {
           reject( xhr.status + ': ' + xhr.statusText );
-        };
+        } else {
+          let data = JSON.parse(xhr.responseText);
 
-        xhr.send();
-      });
+          resolve(data);
+        }
+      };
+
+      xhr.onerror = () => {
+        reject( xhr.status + ': ' + xhr.statusText );
+      };
+
+      xhr.send();
+    });
 
 
   }
--- js/services/phone-service.js
+++ js/services/phone-service.js
@@ -1,9 +1,16 @@
+const API_BASE_URL = 'http://localhost:8080/server/data/';
+
 class PhoneService {
   static getAll() {
-    return HttpService.sendRequest('phones/phones.json');
+    return fetch(API_BASE_URL + 'phones/phones.json')
+      .then(response => response.json())
+      .catch(error => {
+        console.log('Request failed', error)
+      })
   }
 
   static get(phoneId) {
-    return HttpService.sendRequest(`phones/${ phoneId }.json`);
+    return fetch(API_BASE_URL + `phones/${ phoneId }.json`)
+      .then(response => response.json());
   }
 }
